64dfd37ee9a5b0a873db010a9d182efea12a459e
pbowman0 <paigeelisabethbowman@gmail.com>
2021-11-03 10:29:16 -0500

    Initial Commit

---
+++ GroupProjectPickADriver/GroupProjectPickADriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupProjectPickADriverApp.swift
+//  GroupProjectPickADriver
+//
+//  Created by Student on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct GroupProjectPickADriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
