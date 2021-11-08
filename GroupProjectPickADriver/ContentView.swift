9890e53722a53819f341dd17d9da110d8833e5e5
Colin Farley <colinfarley@yahoo.com>
2021-11-08 10:35:06 -0600

    classes 1-8 arrays, classes 1-8 text fields

--- GroupProjectPickADriver/ContentView.swift
+++ GroupProjectPickADriver/ContentView.swift
@@ -6,11 +6,85 @@
 //
 
 import SwiftUI
-
+var studentNames1 = [""]
+var studentNames2 = [""]
+var studentNames3 = [""]
+var studentNames4 = [""]
+var studentNames5 = [""]
+var studentNames6 = [""]
+var studentNames7 = [""]
+var studentNames8 = [""]
 struct ContentView: View {
+    @State private var isEditing = false
+    @State private var studentName = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section(header: Text("Class One")) {
+                TextField("Please enter student name", text: $studentName) { isEditing in
+                    self.isEditing = isEditing
+                }
+                onCommit: {
+                    studentNames1.append(studentName)
+                }
+                     }
+            Section(header: Text("Class Two")) {
+                TextField("Please enter student name", text: $studentName) { isEditing in
+                    self.isEditing = isEditing
+                }
+                onCommit: {
+                    studentNames2.append(studentName)
+                }
+                     }
+            Section(header: Text("Class Three")) {
+                TextField("Please enter student name", text: $studentName) { isEditing in
+                    self.isEditing = isEditing
+                }
+                onCommit: {
+                    studentNames3.append(studentName)
+                }
+                     }
+            Section(header: Text("Class Four")) {
+                TextField("Please enter student name", text: $studentName) { isEditing in
+                    self.isEditing = isEditing
+                }
+                onCommit: {
+                    studentNames4.append(studentName)
+                }
+                     }
+            Section(header: Text("Class Five")) {
+                TextField("Please enter student name", text: $studentName) { isEditing in
+                    self.isEditing = isEditing
+                }
+                onCommit: {
+                    studentNames5.append(studentName)
+                }
+                     }
+            Section(header: Text("Class Six")) {
+                TextField("Please enter student name", text: $studentName) { isEditing in
+                    self.isEditing = isEditing
+                }
+                onCommit: {
+                    studentNames6.append(studentName)
+                }
+                     }
+            Section(header: Text("Class Seven")) {
+                TextField("Please enter student name", text: $studentName) { isEditing in
+                    self.isEditing = isEditing
+                }
+                onCommit: {
+                    studentNames7.append(studentName)
+                }
+                     }
+            Section(header: Text("Class Eight")) {
+                TextField("Please enter student name", text: $studentName) { isEditing in
+                    self.isEditing = isEditing
+                }
+                onCommit: {
+                    studentNames8.append(studentName)
+                }
+                     }
+        }
+        
     }
 }
 
